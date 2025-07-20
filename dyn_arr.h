6e0160fa59d454af169b1c9df9c535174501ce72
Julian Kliebhan <hallo@jkliebhan.de>
2025-07-20 11:38:39 +0200

    added Insertion, renamed Insert to Set, added ForEach

--- dyn_arr.h
+++ dyn_arr.h
@@ -11,8 +11,6 @@ typedef struct {
     uint64_t cursor;
     void* start; 
 } Dyn; 
-
-
 static inline int Init_dyn(Dyn *dyn, size_t size){
     dyn->count = 10;
     dyn->size = size;
@@ -35,15 +33,33 @@ static inline void Push_dyn(Dyn *dyn, void *value){
     memcpy(insert, value, dyn->size);
     return;
 }
-static inline int Insert_dyn(Dyn *dyn, uint64_t place, void *value){
+static inline int Set_dyn(Dyn *dyn, uint64_t place, void *value){
     if(place >= dyn->count) return -1;
     void *insert = dyn->start + (dyn->size) * place;
     memcpy(insert, value, dyn->size);
     return 0;
 }
-static inline void* Get_dyn(Dyn *dyn, uint64_t place){
+static inline int Insert_dyn(Dyn *dyn, uint64_t place, void *value){
+    if(place >= dyn->count) return -1; 
+    if(dyn->count <= dyn->cursor +1) Grow_dyn(dyn);
+    void *insert = dyn->start + (dyn->size) * place;
+    int amount = dyn->cursor - place;
+    memcpy(insert + dyn->size, insert, dyn->size * amount);
+    dyn->cursor += 1;
+    memcpy(insert, value, dyn->size);
+    return 0;
+}
+static inline void *Get_dyn(Dyn *dyn, uint64_t place){
     if(place >= dyn->cursor) return NULL;
     return dyn->start + (dyn->size) * place;
     
+}
+static inline void *For_each_dyn(Dyn *dyn, int (*func)(void*, void*, int64_t), void *func_args){
+    for(int64_t i = 0; i < dyn->cursor; i += 1) {
+        void *iterator = dyn->start + (dyn->size * i);
+        int return_value = func(iterator, func_args, i);
+        if(return_value != 0) return NULL;
+    }
+    return func_args;
 }
 #endif
--- dyn_arr_demo.c
+++ dyn_arr_demo.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <inttypes.h>
+
 #include "dyn_arr.h"
 
+
+// Custom Struct Demo
+typedef struct {
+    int age;
+    const char* name;
+} Person;
+
+int print_person(void *iterator, void *arg, int64_t pos){
+    Person person = *(Person*) iterator;
+    printf("%lld. %s is %d old\n", pos, person.name, person.age);
+    return 0;
+}
+
 int main(int argc, char** argv) {
-    printf("--- dynamic array demo 0.1 ---\n");    
+    
+    int person_count = 20;
+    if(argc > 1) person_count = (int) strtoimax(argv[1], NULL, 10);
+
+    srand(time(NULL));
+    printf("--- dynamic array demo ---\n");    
     // Integer Demo
     Dyn int_dyn;
     Init_dyn(&int_dyn, sizeof(int));
@@ -12,18 +36,14 @@ int main(int argc, char** argv) {
         int *arr = int_dyn.start; 
         printf("index %d value %d\n", i, arr[i]); 
     }
+    printf("---------- Set -----------\n");
     
     int insert = 69; 
-    Insert_dyn(&int_dyn, 98, &insert);
+    Set_dyn(&int_dyn, 98, &insert);
     int *int_arr = int_dyn.start; 
     printf("index %d value %d\n", 98, int_arr[98]); 
     
-    // Custom Struct Demo
-    typedef struct {
-        int age;
-        const char* name;
-    } Person;
-
+    //Costum Struct Demo
     Dyn person_dyn;
     Init_dyn(&person_dyn, sizeof(Person));
     
@@ -34,16 +54,29 @@ int main(int argc, char** argv) {
         "Paul", "Silke", "Thomas", "Ute", "Volker"
     };
     
-    for(int i = 0; i < 100; i += 1) {
+    printf("---------- Custom struct -----------\n");
+    for(int i = 0; i < person_count; i += 1) {
         Person person = {.age = i, .name = germanNames[rand() % 20] };
         Push_dyn(&person_dyn, &person);
         Person *person_arr = person_dyn.start;
         printf("index %d age %d name %s\n", i, person_arr[i].age, person_arr[i].name); 
     }
+    int rand_person = rand() % person_count;
+    Person *person = Get_dyn(&person_dyn, rand_person);
+    printf("---------- Get -----------\n");
+    printf("index %d age %d name %s\n", rand_person, person->age, person->name); 
 
-    Person *person = Get_dyn(&person_dyn, 69);
+    printf("---------- Insertion -----------\n");
+    Person a_person = {.age = 1234, .name = "####Inserted####" };
+    int rand_insertion_place = rand() % person_count;
+    Insert_dyn(&person_dyn, rand_insertion_place, &a_person);
     Person *person_arr = person_dyn.start;
-    printf("index %d age %d name %s\n", 69, person_arr[69].age, person_arr[69].name); 
+    for(int i = 0; i < person_dyn.cursor; i += 1) {
+        printf("index %d age %d name %s\n", i, person_arr[i].age, person_arr[i].name); 
+    }
+    
+    printf("--------- For Each ---------- \n");
+    For_each_dyn(&person_dyn, print_person, NULL); 
 
     return 0;
 }
